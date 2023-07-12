4cbfe936bbb29fb8e430ab993439569f34f2d284
Roald Ong <roald.ong@gmail.com>
2023-07-12 11:03:43 +0800

    Added in slam boolean flag and custom rviz config

--- launch/master.launch.py
+++ launch/master.launch.py
@@ -5,7 +5,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from launch.actions import SetEnvironmentVariable
 from launch.conditions import IfCondition, UnlessCondition
@@ -17,20 +17,20 @@ def generate_launch_description():
     current_dir = get_package_share_directory("warehousebot_navigation")
     twist_mux_dir = get_package_share_directory("twist_mux")
     joy_teleop_dir = get_package_share_directory("teleop_twist_joy")
-    rviz_config_dir = LaunchConfiguration("rviz_config_dir")
 
     ld = LaunchDescription()
 
-    run_rviz2 = LaunchConfiguration("run_rviz2")
+    slam = LaunchConfiguration("slam")
+    rviz_nav_config_dir = LaunchConfiguration("rviz_nav_config_dir")
 
-    declare_rviz_launch = DeclareLaunchArgument(
-        "run_rviz2", default_value="false", description="run rviz"
+    declare_rviz_nav_config = DeclareLaunchArgument(
+        "rviz_nav_config_dir",
+        default_value=os.path.join(current_dir, "rviz", "live_nav.rviz"),
+        description="default path to rviz nav config file",
     )
 
-    declare_rviz_config = DeclareLaunchArgument(
-        "rviz_config_dir",
-        default_value=os.path.join(current_dir, "rviz", "live_nav.rviz"),
-        description="default path to rviz config file",
+    declare_slam_cmd = DeclareLaunchArgument(
+        "slam", default_value="False", description="Whether run SLAM"
     )
 
     # launch twist mux
@@ -50,12 +50,12 @@ def generate_launch_description():
     )
 
     # launch rviz2
-    launch_rviz2 = Node(
-        condition=IfCondition(run_rviz2),
+    launch_rviz2_nav = Node(
+        condition=IfCondition(PythonExpression(["not ", slam])),
         package="rviz2",
         executable="rviz2",
         name="rviz2",
-        arguments=["-d", rviz_config_dir],
+        arguments=["-d", rviz_nav_config_dir],
         output="screen",
     )
 
@@ -67,10 +67,10 @@ def generate_launch_description():
     )
 
     ld.add_action(launch_twist_mux)
-    ld.add_action(declare_rviz_launch)
-    ld.add_action(declare_rviz_config)
+    ld.add_action(declare_rviz_nav_config)
+    ld.add_action(declare_slam_cmd)
     ld.add_action(launch_twist_joy)
-    ld.add_action(launch_rviz2)
+    ld.add_action(launch_rviz2_nav)
     ld.add_action(launch_nav2)
 
     return ld
